a5a078cc7d99172fc7daff475b8cbdb77bf7e960
KOOSANGYOON <nevergiveup936@naver.com>
2017-11-20 17:06:37 +0900

    Update some features
    1. input form is changed. (ex) 7,8
    2. if user input "7,8", result data will be "2X1, 2X2, ... , 2X8, 3X1, ... , 7X8)

--- src/multipleTable/MultipleTable.java
+++ src/multipleTable/MultipleTable.java
@@ -3,55 +3,42 @@ package multipleTable;
 import java.util.Scanner;
 
 public class MultipleTable {
-	//정수를 입력받는 메소드 -> checkNum 메소드로 입력받은 정수를 보내준다.
-	//입력 받은 정수의 값이 오류가 있을 때, -1을 리턴해준다.
-	public static int takeNum() {
-		System.out.println("구구단 중 출력할 단은? : ");
-        Scanner scanNum = new Scanner(System.in);
-        int number = scanNum.nextInt();
-        int inputNum = checkNum(number);
-        scanNum.close();
-        if (inputNum == -1) {
-        		return -1;
-        }
-        return inputNum;
+	//입력받은 두 정수의 크기를 확인하는 메소드 -> 둘 중 하나라도 0보다 작으면 false 리턴.
+	public static boolean takeNum(int firstNum, int secondNum) {
+//        if (firstNum < 0 || secondNum < 0) {
+//        		return false;
+//        }
+//        	return true;
+		return firstNum > 0 && secondNum > 0;
 	}
-	//정수를 입력 받아서, 2보다 작거나 9보다 크면 -1을 리턴해준다. 그렇지 않으면 정수 그대로를 리턴해준다.
-	public static int checkNum(int number) {
-		if (number < 2) {
-			System.out.println("2보다 커야합니다.\n");
-			return -1;
-		}else if (number > 9) {
-			System.out.println("9보다 작아야 합니다.\n");
-			return -1;
-		}else {
-			return number;
+	
+	//입력받은 정수를 계산하는 메소드. 계산 값을 화면에 출력해준다.
+	public static void calculate(int first, int second) {
+		for (int i = 2; i <= first; i++) {
+			System.out.println(i + " 단 출력");
+			for (int j = 1; j <= second; j++) {
+				System.out.println(i + " * " + j + " = " + (i*j));
+			}
 		}
 	}
-	//입력받은 정수를 X 1, X 2, ... , X 9 까지 계산하여 배열의 한 칸마다 입력해준다.
-	public static int[] calculate(int num) {
-		int[] result = new int[9];
-		
-		for (int i = 0; i < result.length; i++) {
-			result[i] = num * (i + 1);
-		}
-		return result;
-	}
-	//입력받은 배열의 첫 원소부터 끝 원소까지 차례로 출력해준다.
-	public static void printResult(int[] result) {
-		System.out.println(result[0] + "단 의 결과입니다.\n");
-		for (int i = 0; i < result.length; i++) {
-			System.out.println(result[0] + " * " + (i + 1) + " = " + result[i]);
-		}
-	}
+	
 	//main start
 	public static void main(String[] args) {
-		int inputNum = takeNum();
-		if (inputNum == -1) {		//리턴 받은 값이 -1일 경, 데러 메시지 출력해주고 종료.
-			System.out.println("다시 시작해 주세요.\n");
+		System.out.println("구구단 몇 몇 단을 알려드릴까요?\n (ex) 7,8 = 2X1, 2X2, ... , 2X9, 3X1, ... , 7X1, 7X2, ... , 7X8 \n 입력 : ");
+        
+		Scanner scanData = new Scanner(System.in);
+        String inputData = scanData.nextLine();
+        String[] inputArray = inputData.split(",");		// ',' 를 기준으로 좌 우 값을 배열에 넣어준다.
+        scanData.close();
+        
+        int firstNum = Integer.parseInt(inputArray[0]);		//배열에 들어간 두 값을 정수형으로 변환하여 준다.
+        int secondNum = Integer.parseInt(inputArray[1]);
+        
+		boolean isInputRight = takeNum(firstNum, secondNum);
+		if (isInputRight == false) {		//종료 조건 1 = 두 값 중에 0보다 작은 값이 하나라도 있으면 종료.
+			System.out.println("두 수 모두 양수를 입력해야 합니다. 다시 시작해 주세요.\n");
 			return;
 		}
-		int[] resultArray = calculate(inputNum);
-		printResult(resultArray);
+		calculate(firstNum, secondNum);
 	}
 }

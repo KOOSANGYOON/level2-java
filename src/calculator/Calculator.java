40c12b55db26267b0cad0de815f786d9221e4a06
KOOSANGYOON <nevergiveup936@naver.com>
2017-11-21 13:44:36 +0900

    Update some features
    1. add remarks all methods
    2. edit some messages`

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -2,21 +2,7 @@ package calculator;
 import java.util.Scanner;
 
 public class Calculator {
-//	public static int takeData(Scanner data) {
-//		System.out.println("값을 입력하세요.");
-//		String inputString = data.nextLine();
-//		int returnData = Integer.parseInt(inputString);
-//		
-//		return returnData;
-//	}
-//	
-//	public static char takeSymbol(Scanner data) {
-//		System.out.println("연산자를 입력하세요. (ex. '+', '-', '*', '/', '%', 'c')");
-//		char inputSym = data.next().charAt(0);
-//		
-//		return inputSym;
-//	}
-	
+	//받은 입력 데이터 2개와, 연산자를 기준으로 계산하여 결과값을 리턴해주는 메소드.
 	public static int calculateData(int firstNum, int secondNum, char sym) {
 		int returnResult = 0;
 		
@@ -35,32 +21,32 @@ public class Calculator {
 	}
 	
 	public static void main(String[] args) {
-		String result = "";
-		int resultData = 0;
+		String result = "";		//매 순간순간마다 현재 수식을 출력해 줄 result 값 초기화
+		int resultData = 0;		//총 계산 결과를 0으로 초기화
 		Scanner firstInput = new Scanner(System.in);
 		int inputData = Input.takeData(firstInput);
 		
 		result += inputData;
 		resultData = inputData;
 		System.out.println("총 수식 : " + result);
-		System.out.println("계산 결과값 : " + resultData);
+		System.out.println("계산 결과값(현재값) : " + resultData);
 		
-		while (true) {
+		while (true) {		//연산자 'c' (clear) 를 입력받을 때 까지 계속해서 반복.
 			Scanner symbol = new Scanner(System.in);
 			Scanner nextData = new Scanner(System.in);
 			char inputSymbol = Input.takeSymbol(symbol);
 			if (inputSymbol == 'c') {
-				System.out.println("EXIT");
+				System.out.println("CLEAR AND EXIT");
 				break;
 			}
 			result += inputSymbol;
 			System.out.println("총 수 : " + result);
-			System.out.println("계산 결과값 : " + resultData);
+			System.out.println("계산 결과값(현재값) : " + resultData);
 			inputData = Input.takeData(nextData);
 			resultData = calculateData(resultData, inputData, inputSymbol);
 			result += inputData;
 			System.out.println("총 수식 : " + result);
-			System.out.println("계산 결과값 : " + resultData);
+			System.out.println("계산 결과값(현재값) : " + resultData);
 		}
 		firstInput.close();
 	}
--- src/calculator/Input.java
+++ src/calculator/Input.java
@@ -2,8 +2,9 @@ package calculator;
 
 import java.util.Scanner;
 
+//입력값을 받아서 값은 숫자로 바꿔주고, 연산자는 기호 그대로 리턴해주는 클래스
 public class Input {
-	static int takeData(Scanner data) {
+	static int takeData(Scanner data) {		//data를 받으면,
 		System.out.println("값을 입력하세요.");
 		String inputString = data.nextLine();
 		int returnData = Integer.parseInt(inputString);
@@ -11,7 +12,7 @@ public class Input {
 		return returnData;
 	}
 	
-	public static char takeSymbol(Scanner data) {
+	public static char takeSymbol(Scanner data) {		//연산자를 받으면,
 		System.out.println("연산자를 입력하세요. (ex. '+', '-', '*', '/', '%', 'c')");
 		char inputSym = data.next().charAt(0);
 		
